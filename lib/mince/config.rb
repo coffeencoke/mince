afca31d52a8730d585f3635166916479d659236f
Michael K Schmidt + Matt Simpson <ionic-mobile-dev+mks+ms@ace-01.asolutions.com>
2012-09-17 15:26:45 -0500

    Fix issue with namespace

--- lib/mince/config.rb
+++ lib/mince/config.rb
@@ -1,24 +1,26 @@
 require 'active_support/core_ext/object/blank'
 require 'singleton'
 
-class Mince::Config
-  include Singleton
+module Mince
+  class Config
+    include Singleton
 
-  attr_reader :database_name
+    attr_reader :database_name
 
-  def initialize
-    self.database_name = "mince"
-  end
+    def initialize
+      self.database_name = "mince"
+    end
 
-  def database_name=(val)
-    @database_name = [val, ENV['TEST_ENV_NUMBER']].compact.join("-")
-  end
+    def database_name=(val)
+      @database_name = [val, ENV['TEST_ENV_NUMBER']].compact.join("-")
+    end
 
-  def self.database_name
-    instance.database_name
-  end
+    def self.database_name
+      instance.database_name
+    end
 
-  def self.database_name=(val)
-    instance.database_name = val
+    def self.database_name=(val)
+      instance.database_name = val
+    end
   end
 end
