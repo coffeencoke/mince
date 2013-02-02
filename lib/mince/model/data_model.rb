c21fb9bd40dc56e333914dc6af858532593c69ba
Matt Simpson <matt.simpson3@gmail.com>
2013-02-02 11:19:32 -0600

    Move a method to a different module

--- lib/mince/model/data_model.rb
+++ lib/mince/model/data_model.rb
@@ -12,6 +12,20 @@ module Mince
           @data_model
         end
       end
+
+      protected
+
+      # Ensures that the data model has all of the fields that are trying to be saved. Raises an
+      # exception if the data model does not.
+      #
+      # Not sure if this is where this method should live, it requires both
+      # Mince::Model::DataModel and Mince::Model::Fields.
+      def ensure_no_extra_fields
+        extra_fields = (fields - data_model.data_fields)
+        if extra_fields.any?
+          raise "Tried to save a #{self.class.name} with fields not specified in #{data_model.name}: #{extra_fields.join(', ')}"
+        end
+      end
     end
   end
 end
